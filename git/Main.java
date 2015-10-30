8c16b208d533444aaf4748521471969c80af4ba1
KimGyeongA <whenas@nate.com>
2015-10-30 23:58:56 +0900

    View Main 수정

---
+++ git/Main.java
@@ -0,0 +1,37 @@
+package sw_engineering.project01;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	static DBConnection dbConnection = null;
+	static AddStudent addStudent = null;
+	static ViewStudent viewStudent = null;
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		dbConnection = new DBConnection();
+		int choice;
+		do {
+			System.out.println("======================");
+			System.out.println(" 1. 학생 추가 ");
+			System.out.println(" 2. 학생 삭제 ");
+			System.out.println(" 3. 학생 정보 수정 ");
+			System.out.println(" 4. 학생 정보 열람 ");
+			System.out.println("======================");
+			System.out.print("번호를 선택하세요 >> ");
+			Scanner scan = new Scanner(System.in);
+			choice = scan.nextInt();
+		
+			switch(choice) {
+			case 1:
+				addStudent = new AddStudent(DBConnection.connection);
+				break;
+			case 4:
+				viewStudent = new ViewStudent(DBConnection.connection);
+				break;
+			}
+		} while (choice < 1 || choice > 4);
+		dbConnection.disconnectDatabase();
+	}
+}

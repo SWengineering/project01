55a07b778b7a7bdfad998c1b11df582b65532c4e
sthl <sthl@naver.com>
2015-10-31 18:18:56 +0900

    유진 1차 업데이트(파일2개)

--- DBConnection.java
+++ DBConnection.java
@@ -3,13 +3,11 @@ package sw_engineering.project01;
 import java.sql.*;
 
 public class DBConnection {
-	static String url = "jdbc:mysql://localhost:3306/student_info";
-	static String user = "root";
-	static String password = "1234";
+	final static String url = "jdbc:mysql://localhost:3306/student_info";
+	final static String user = "root";
+	final static String password = "1234";
 	
 	public static Connection connection = null;
-	public static Statement statement = null;
-	public static ResultSet result = null;
 
 	public DBConnection() {
 		if( loadDriver() )
@@ -19,7 +17,7 @@ public class DBConnection {
 	public static boolean loadDriver() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			//System.out.println("JDBC 드라이버 로드 성공!");
+			System.out.println("JDBC 드라이버 로드 성공!");
 			
 			return true;
 		} catch (java.lang.ClassNotFoundException e) {
@@ -35,35 +33,13 @@ public class DBConnection {
 			connection = DriverManager.getConnection(url, user, password);
 			System.out.println("\n데이터베이스 연결 성공: "+ url);
 			
-			statement = connection.createStatement();
-			
-//			//test
-//			//-------------------------------------------------------------
-//			String sql = "Select * From student_info.student";
-//			result = statement.executeQuery(sql);
-//			
-//			while (result.next()) {
-//				int id = result.getInt("id");
-//				String name = result.getString("name");
-//				String department = result.getString("department");
-//				String phone = result.getString("phone");
-//				System.out.println(id+"\t"+name+"\t"+department+"\t"+phone);
-//			}
-//			//-------------------------------------------------------------
-			
 		} catch (SQLException ex) {
 			System.out.println("\n데이터베이스 연결 실패: "+ url);
 		}
 	}
 	
 	public void disconnectDatabase() {
-		try {
-			if(result != null)	
-				result.close();
-			
-			if(statement != null)	
-				statement.close();
-			
+		try {			
 			if(connection != null)
 				connection.close();
 			

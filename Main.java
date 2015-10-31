3863d6e170b29bc02c3ce1960414172bc4cd3b44
sykim405 <kow018@naver.com>
2015-10-31 22:46:01 +0900

    불필요한 부분 제거

--- Main.java
+++ Main.java
@@ -16,11 +16,11 @@ public class Main {
 
 		int choice = 1;
 		
-<<<<<<< HEAD
+
 		// 추가할 부분!!	
 		if (DBConnection.connection != null)
 			addStudent = new AddStudent(DBConnection.connection);
-=======
+
 		do {
 			System.out.println("======================");
 			System.out.println(" 1. 학생 추가 ");
@@ -53,7 +53,6 @@ public class Main {
 				return;
 			}
 		} while ((choice >= 1) && (choice <= 4));
->>>>>>> d8f2dc5769f345b1944aa59db21d624f64159d9a
 		
 		dbConnection.disconnectDatabase();
 	}

d8f2dc5769f345b1944aa59db21d624f64159d9a
shinso <jtlsth@gmail.com>
2015-10-31 20:24:09 +0900

    delete기능 구현 및 기존파일 오류 수정

--- Main.java
+++ Main.java
@@ -6,16 +6,14 @@ import java.util.Scanner;
 public class Main {
 	static DBConnection dbConnection = null;
 	static AddStudent addStudent = null;
-<<<<<<< HEAD
+	static DeleteStudent deleteStudent = null;
 	static UpdateStudent updateStudent = null;
-=======
->>>>>>> 9cc385cc5ec4dc477310d34af267875da3b1ca87
 	static ViewStudent viewStudent = null;
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		dbConnection = new DBConnection();
-<<<<<<< HEAD
+
 		int choice = 1;
 		
 		do {
@@ -30,27 +28,14 @@ public class Main {
 			
 			Scanner scan = new Scanner(System.in);
 			choice = scan.nextInt();
-		
-=======
-		int choice;
-		do {
-			System.out.println("======================");
-			System.out.println(" 1. 학생 추가 ");
-			System.out.println(" 2. 학생 삭제 ");
-			System.out.println(" 3. 학생 정보 수정 ");
-			System.out.println(" 4. 학생 정보 열람 ");
-			System.out.println("======================");
-			System.out.print("번호를 선택하세요 >> ");
-			Scanner scan = new Scanner(System.in);
-			choice = scan.nextInt();
-		
->>>>>>> 9cc385cc5ec4dc477310d34af267875da3b1ca87
+
 			switch(choice) {
 			case 1:
 				addStudent = new AddStudent(DBConnection.connection);
 				break;
-<<<<<<< HEAD
 			case 2:
+				deleteStudent = new DeleteStudent(DBConnection.connection);
+				break;
 			case 3:
 				updateStudent = new UpdateStudent(DBConnection.connection);
 				break;
@@ -64,13 +49,6 @@ public class Main {
 			}
 		} while ((choice >= 1) && (choice <= 4));
 		
-=======
-			case 4:
-				viewStudent = new ViewStudent(DBConnection.connection);
-				break;
-			}
-		} while (choice < 1 || choice > 4);
 		dbConnection.disconnectDatabase();
->>>>>>> 9cc385cc5ec4dc477310d34af267875da3b1ca87
 	}
 }

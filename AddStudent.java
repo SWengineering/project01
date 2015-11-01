833cf6f88ea7f49f4b373b28f051cd5048d9ac68
sthl <sthl@naver.com>
2015-11-02 00:09:08 +0900

    데이터베이스 내용 없을시 문제 부분 해결

--- AddStudent.java
+++ AddStudent.java
@@ -13,7 +13,9 @@ public class AddStudent {
 	String phone;
 	
 	public AddStudent(Connection connection) {
-		insertStudent(connection);
+		if( getInputFromUser() ) {
+			insertStudent(connection);
+		}
 	}
 
 	public void insertStudent(Connection connection) {
@@ -21,12 +23,26 @@ public class AddStudent {
 		
 		try {
 			statement = connection.prepareStatement(sql);
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-<<<<<<< HEAD
 			
-=======
->>>>>>> 9cc385cc5ec4dc477310d34af267875da3b1ca87
+			statement.setString(1, studentId);
+			statement.setString(2, name);
+			statement.setString(3, department);
+			statement.setString(4, phone);
+			
+			statement.executeUpdate();
+			System.out.println("학생 추가 성공");
+			
+		} catch(Exception e) {
+			System.out.println("학생 추가 실패");
+		}
+	}
+
+	public boolean getInputFromUser() {
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
 			System.out.println("*** Student 테이블에 학생 추가하기 ***");
+			
 			System.out.print("학번 입력 : ");
 			studentId = bufferedReader.readLine();
 			System.out.print("이름 입력 : ");
@@ -36,27 +52,11 @@ public class AddStudent {
 			System.out.print("연락처 입력 : ");
 			phone = bufferedReader.readLine();
 			
-			statement.setString(1, studentId);
-			statement.setString(2, name);
-			statement.setString(3, department);
-			statement.setString(4, phone);
-			//sql += "('" + studentId + "', '" + name + "', '" + department + "', '" + phone + "')";
-			//System.out.println(sql);
-			
-			statement.executeUpdate();
-			System.out.println("학생 추가 성공");
-			
+			return true;
 		} catch(Exception e) {
-			System.out.println("학생 추가 실패");
+			System.out.println("사용자 입력 실패");
 		}
+		
+		return false;
 	}
-	/*
-	String sql = "insert into albums values(?,?,?,?)";
-	PreparedStatement stat=conn.prepareStatement(sql);
-	stat.setString(1, vaname);
-	stat.setString(2, vbname);
-	stat.setString(3, vyear);
-	stat.setString(4, vprice);
-	stat.executeUpdate();
-	*/
 }
--- Main.java
+++ Main.java
@@ -6,18 +6,18 @@ import java.util.Scanner;
 public class Main {
 	static DBConnection dbConnection = null;
 	static AddStudent addStudent = null;
-<<<<<<< HEAD
+	static DeleteStudent deleteStudent = null;
 	static UpdateStudent updateStudent = null;
-=======
->>>>>>> 9cc385cc5ec4dc477310d34af267875da3b1ca87
 	static ViewStudent viewStudent = null;
+	static int choice = 0;
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		dbConnection = new DBConnection();
-<<<<<<< HEAD
-		int choice = 1;
-		
+		dbConnection = new DBConnection();		
+
+		if (DBConnection.connection != null)
+			addStudent = new AddStudent(DBConnection.connection);
+
 		do {
 			System.out.println("======================");
 			System.out.println(" 1. 학생 추가 ");
@@ -30,27 +30,14 @@ public class Main {
 			
 			Scanner scan = new Scanner(System.in);
 			choice = scan.nextInt();
-		
-=======
-		int choice;
-		do {
-			System.out.println("======================");
-			System.out.println(" 1. 학생 추가 ");
-			System.out.println(" 2. 학생 삭제 ");
-			System.out.println(" 3. 학생 정보 수정 ");
-			System.out.println(" 4. 학생 정보 열람 ");
-			System.out.println("======================");
-			System.out.print("번호를 선택하세요 >> ");
-			Scanner scan = new Scanner(System.in);
-			choice = scan.nextInt();
-		
->>>>>>> 9cc385cc5ec4dc477310d34af267875da3b1ca87
+
 			switch(choice) {
 			case 1:
 				addStudent = new AddStudent(DBConnection.connection);
 				break;
-<<<<<<< HEAD
 			case 2:
+				deleteStudent = new DeleteStudent(DBConnection.connection);
+				break;
 			case 3:
 				updateStudent = new UpdateStudent(DBConnection.connection);
 				break;
@@ -62,15 +49,15 @@ public class Main {
 				System.out.println("프로그램을 종료합니다.");
 				return;
 			}
-		} while ((choice >= 1) && (choice <= 4));
+		} while ( checkUserSelection(choice) );
 		
-=======
-			case 4:
-				viewStudent = new ViewStudent(DBConnection.connection);
-				break;
-			}
-		} while (choice < 1 || choice > 4);
 		dbConnection.disconnectDatabase();
->>>>>>> 9cc385cc5ec4dc477310d34af267875da3b1ca87
+	}
+
+	public static boolean checkUserSelection(int choice) {
+		if ((choice >= 1) && (choice <= 4))
+			return true;
+
+		return false;
 	}
 }
